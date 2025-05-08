573b88b346d2122ec59342222b03ab9dac13a379
Kasrym <theseraphkasrym@gmail.com>
2025-05-08 13:56:35 -0500

    Added groups and player, and circleshape classes

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
+from player import Player
 import pygame
 from constants import *
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        updatable.update(dt)
+        
+        
         screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen) 
+
         pygame.display.flip()
+        dt = clock.tick() / 1000
 
 
 
